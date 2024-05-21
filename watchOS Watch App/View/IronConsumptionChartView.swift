a2c1bf420f38501cb28d4c71862685622855937e
Jennifer <57284701+JennLuv@users.noreply.github.com>
2024-05-22 01:15:58 +0700

    working with health kit and swift charts

---
+++ watchOS Watch App/View/IronConsumptionChartView.swift
@@ -0,0 +1,18 @@
+//
+//  IronConsumptionChartView.swift
+//  watchOS Watch App
+//
+//  Created by Jennifer Luvindi on 21/05/24.
+//
+
+import SwiftUI
+
+struct IronConsumptionChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IronConsumptionChartView()
+}
